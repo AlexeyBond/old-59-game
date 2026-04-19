b835025b387e24a8ae26b2b27f49c74b344c6714
AlexeyBond <alexey.bond.94.55@gmail.com>
2026-04-20 02:01:51 +0600

    inputs, exports

--- globals.gd
+++ globals.gd
@@ -3,7 +3,12 @@ extends Node
 @export_file_path("*.tscn") var start_level: String
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("reset_all"):
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
+	elif event.is_action_pressed("reset_all"):
 		get_tree().change_scene_to_file(start_level)
 	elif event.is_action_pressed("reset_level"):
 		get_tree().reload_current_scene()
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
